6da6273cf79568d42de623ce3676d8e3374af083
张法洋 <zhangfy@csdn.net>
2025-01-20 11:31:13 +0800

    app api

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Vercel Flask!'
+@app.route('/api')
+def hello_world():
+    return 'Hello, vercel api!'
 
 if __name__ == '__main__':
     app.run()
