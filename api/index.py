3ee864d4f695ea0f5d3021c7a8f9d8a220059408
张法洋 <zhangfy@csdn.net>
2025-01-20 11:13:09 +0800

    vercel demo

--- api/index.py
+++ api/index.py
@@ -1,34 +1,10 @@
-# from flask import Flask, jsonify, request
-# from vercel.wsgi import VercelWSGI
-
-# app = Flask(__name__)
-
-# @app.route('/api')
-# def hello():
-#     return jsonify({"message": "Hello from Flask on Vercel!"})
-
-# @app.route('/api/data', methods=['POST'])
-# def get_data():
-#     data = request.get_json()
-#     # 在这里处理POST请求的数据
-#     return jsonify({"received": data})
-
-# # 重点：使用 VercelWSGI 适配器
-# app_wsgi = VercelWSGI(app)
-
-# def main(request):
-#     return app_wsgi(request)
-
-from flask import Flask, jsonify
-from vercel.wsgi import VercelWSGI
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api')
-def hello():
-    return jsonify({"message": "Hello from Flask on Vercel!"})
+@app.route('/')
+def hello_world():
+    return 'Hello, Vercel Flask!'
 
-app_wsgi = VercelWSGI(app)
-
-def main(request):
-    return app_wsgi(request)
+if __name__ == '__main__':
+    app.run()
